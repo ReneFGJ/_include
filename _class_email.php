b657092389a39c27b799fa4dda2b7a5bff9a8a74
Rene FGJr <renefgj@gmail.com>
2014-11-21 11:17:31 -0200

    v0.14.45b

--- _class_email.php
+++ _class_email.php
@@ -11,13 +11,17 @@
 
  /* Verifica se não existe esta funcao criada */
 if (!function_exists('enviaremail')) {
-	function enviaremail($a1 = '', $a2 = '', $a3 = '', $a4 = '', $a5 = '', $a6 = '', $a7 = '') {
-		echo '<BR>A1=' . $a1;
-		echo '<BR>A2=' . $a2;
-		echo '<BR>A3=' . $a3;
-		echo '<BR>A4=' . $a4;
-		echo '<BR>A5=' . $a5;
-		echo '<BR>A6=' . $a6;
+	function enviaremail($to = '', $a2 = '', $subject = '', $messagem = '', $a5 = '', $a6 = '', $a7 = '') {
+		global $hd;
+
+		$em = new email;
+		$em -> user_email = $hd->email;
+		$em -> user_name = $hd->email_name;
+		$em -> user_password  = $hd->email_pass;
+		$em -> user_smtp  = $hd->email_smtp;
+		$em -> method  = 'A';	
+		
+		$em->enviar_email($to, $subject, $messagem, $to_name);
 	}
 
 }
@@ -25,6 +29,7 @@ class email {
 	var $user_email = '';
 	var $user_name = '';
 	var $user_password = '';
+	var $user_smtp = '';
 
 	var $to_email;
 	var $to_name;
@@ -76,6 +81,7 @@ class email {
 	}
 
 	function enviar_email($to = '', $subject = '', $messagem = '', $to_name = '') {
+		global $hd;
 		$this -> to_email = $to;
 		$this -> to_name = $to_name;
 		$this -> subject = $subject;
@@ -144,89 +150,122 @@ class email {
 	}
 
 	function method_autenticado() {
-		restore_error_handler();
-		ini_set('display_errors', 0);
-		ini_set('error_reporting', 0);
-		$usuario = $this -> user_email;
-		$senha = $this -> user_password;
-
-		$assunto = $this -> subject;
-		$nomeDestinatario = trim($this -> to_name);
-
-		if (strlen($this -> replay_name) > 0) {
-			$Remetente_email = trim($this -> replay_email);
-			$Remetente_name = trim($this -> replay_name);
-		} else {
-			$Remetente_email = trim($this -> user_email);
-			$Remetente_name = trim($this -> user_name);
-		}
-		//
-		$destinatarios = $this -> to_email;
-		$tela .= 'xx(tela)';
-		$resposta .= $Remetente_email;
-		$headers = $this -> header();
-		$mensagem = $this -> message . '</B>';
-		if ($this -> monitor == 1) {
-			echo '<fieldset><legend>Usuário que envia</legend>';
-			echo $usuario . ' ' . $senha;
-			echo '</fieldset>';
-			echo '<BR>From: ' . $Remetente_name . ' &lt;' . $Remetente_email . '>';
-			echo '<BR>To:' . $nomeDestinatario . ' &lt;' . $destinatarios . '>';
-		}
-		$_POST['mensagem'] = nl2br($message);
-
-		/***********************************A PARTIR DAQUI NAO ALTERAR************************************/
-		foreach ($_POST as $dados['me1'] => $dados['me2']) { $dados['me3'][] = '<b>' . $dados['me1'] . '</b>: ' . $dados['me2'];
-		}
-
-		$dados['me3'] = $mensagem;
-		$dados['email'] = array('usuario' => $usuario, 'senha' => $senha, 'servidor' => 'smtp.' . substr(strstr($usuario, '@'), 1), 'nomeRemetente' => $Remetente_name, 'nomeDestinatario' => $nomeDestinatario, 'resposta' => $resposta, 'assunto' => $assunto, 'mensagem' => $dados['me3']);
-
-		/* Inicialização */
-		ini_set('php_flag mail_filter', 0);
-		$conexao = fsockopen($dados['email']['servidor'], 587, $errno, $errstr, 10) or die("<font color='red'>ERRO DE ENVIO!</font>");
-		fgets($conexao, 512);
-		$dados['destinatarios'] = explode(',', $destinatarios);
-		/* RCPTTO */
-		foreach ($dados['destinatarios'] as $dados['1']) {
-			$dados['destinatarios']['RCPTTO'][] = '< ' . $dados['1'] . ' >';
-			$dados['destinatarios']['TO'][] = $dados['1'];
-		}
-		/* EHLO */
-		$dados['cabecalho'] = array('EHLO ' => $dados['email']['servidor'], 'AUTH LOGIN', base64_encode($dados['email']['usuario']), base64_encode($dados['email']['senha']), 'MAIL FROM: ' => '< ' . $dados['email']['usuario'] . ' >', 'RCPT TO:' => $dados['destinatarios']['RCPTTO'], 'DATA', 'MIME-Version: ' => '1.0', 'Content-Type: text/html; charset=iso-8859-1', 'Date: ' => date('r', time()), 'From: ' => array($dados['email']['nomeRemetente'] . ' ' => '< ' . $dados['email']['usuario'] . ' >'), 'To:' => array($dados['email']['nomeDestinatario'] . ' ' => $dados['destinatarios']['TO']), 'Reply-To: ' => $dados['email']['resposta'], 'Subject: ' => $dados['email']['assunto'], 'mensagem' => $dados['email']['mensagem'], 'QUIT');
-		foreach ($dados['cabecalho'] as $dados['2'] => $dados['3']) {
-			if (is_array($dados['3'])) {
-				foreach ($dados['3'] as $dados['4'] => $dados['5']) {
-					$dados['4'] = empty($dados['4']) ? '' : $dados['4'];
-					$dados['5'] = empty($dados['5']) ? '' : $dados['5'];
-					$dados['4'] = is_numeric($dados['4']) ? '' : $dados['4'];
-					if (is_array($dados['5'])) { $dados['5'] = "< " . implode(', ', $dados['5']) . " >";
+				restore_error_handler();
+				ini_set('display_errors', 0);
+				ini_set('error_reporting', 0);
+				$usuario = $this->user_email;
+			 	$senha = $this->user_password;
+			 	$Remetente_name = $this->user_name;
+				$Remetente_email = $this->user_email;
+				$smtp_port = $this->smtp_port;
+				//if ($smtp_port == 0) { $smtp_port = 587; }
+				if ($smtp_port == 0) { $smtp_port = 25; }
+				
+				$assunto = $this->subject; 				
+ 				$nomeDestinatario = trim($this->to_name);
+				
+				if (strlen($this->replay_name) > 0)
+					{
+						$Remetente_email = trim($this->replay_email);
+						$Remetente_name = trim($this->replay_name);	
 					}
-					fwrite($conexao, $dados['2'] . $dados['4'] . $dados['5'] . "\r\n", 512) . '<br>';
-					fgets($conexao, 512);
+			//
+			$destinatarios = $this->to_email;
+ 			$tela .= '(tela)';			
+ 			$resposta .= $Remetente_email;			
+			$headers = $this->header();
+			$mensagem = $this->message.'</B>';
+			if ($this->monitor==1)
+				{
+					echo '<fieldset><legend>Usuário que envia</legend>';
+					echo $usuario.' '.$senha;
+					echo '</fieldset>';
+					echo '<BR>From: '.$Remetente_name.' <'.$Remetente_email.'>';
+					echo '<BR>To:'.$nomeDestinatario.' <'.$destinatarios.'>';
 				}
-			} else {
+ 			$_POST['mensagem'] = nl2br($message);
+			 
+ 			/***********************************A PARTIR DAQUI NAO ALTERAR************************************/
+				foreach ($_POST as $dados['me1'] => $dados['me2'])
+				{ $dados['me3'][] = '<b>'.$dados['me1'].'</b>: '.$dados['me2']; }
+			
+				$dados['me3'] = $mensagem;
+				
+				$dados['email'] = array(
+								'usuario' => $usuario, 
+								'senha' => $senha, 
+								'servidor' => $this->user_smtp, 
+								'nomeRemetente' => $Remetente_name, 
+								'nomeDestinatario' => $nomeDestinatario, 
+								'resposta' => $resposta, 
+								'assunto' => $assunto, 
+								'mensagem' => $dados['me3']);
+				
+				/* Inicialização */
+				ini_set('php_flag mail_filter', 0);
+				$conexao = fsockopen($dados['email']['servidor'], $smtp_port, $errno, $errstr, 10) 
+					or die("<font color='red'>ERRO DE ENVIO!</font>");
+				fgets($conexao, 512);
+				$dados['destinatarios'] = explode(',', $destinatarios);
+				/* RCPTTO */
+				foreach ($dados['destinatarios'] as $dados['1'])
+				{
+					$dados['destinatarios']['RCPTTO'][] = '< '.$dados['1'].' >';
+					$dados['destinatarios']['TO'][] = $dados['1'];
+				}
+				/* EHLO */
+				$dados['cabecalho'] = array(
+						'EHLO ' => $dados['email']['servidor'], 
+						'AUTH LOGIN', base64_encode($dados['email']['usuario']), 
+						base64_encode($dados['email']['senha']), 
+						'MAIL FROM: ' => '< '.$dados['email']['usuario'].' >', 
+						'RCPT TO:' => $dados['destinatarios']['RCPTTO'], 
+						'DATA', 
+						'MIME-Version: ' => '1.0', 
+						'Content-Type: text/html; charset=iso-8859-1', 
+						'Date: ' => date('r',time()), 
+						'From: ' => array($dados['email']['nomeRemetente'].' ' => '< '.$dados['email']['usuario'].' >'), 
+						'To:' => array($dados['email']['nomeDestinatario'].' ' => $dados['destinatarios']['TO']), 
+						'Reply-To: ' => $dados['email']['resposta'],
+						'Subject: ' => $dados['email']['assunto'], 
+						'mensagem' => $dados['email']['mensagem'], 
+						'QUIT');
+				foreach ($dados['cabecalho'] as $dados['2'] => $dados['3'])
+				{
+					if (is_array($dados['3']))
+					{ foreach ($dados['3'] as $dados['4'] => $dados['5']) {
+						$dados['4'] = empty($dados['4']) ? '' : $dados['4'];
+						$dados['5'] = empty($dados['5']) ? '' : $dados['5'];
+						$dados['4'] = is_numeric($dados['4']) ? '' : $dados['4'];
+						if (is_array($dados['5']))
+							{ $dados['5'] = "< ".implode(', ', $dados['5'])." >"; }
+						fwrite($conexao, $dados['2'].$dados['4'].$dados['5']."\r\n", 512).'<br>';
+							fgets($conexao, 512);
+					}
+				} else {
 				$dados['2'] = empty($dados['2']) ? '' : $dados['2'];
 				$dados['3'] = empty($dados['3']) ? '' : $dados['3'];
 				$dados['2'] = is_numeric($dados['2']) ? '' : $dados['2'];
-
-				if ($dados['2'] == 'Subject: ') {
-					fwrite($conexao, $dados['2'] . $dados['3'] . "\r\n", 512) . '<br>';
-					fwrite($conexao, "\r\n", 512) . '<br>';
+				
+				if ($dados['2'] == 'Subject: ')
+				{
+					fwrite($conexao, $dados['2'].$dados['3']."\r\n", 512).'<br>';
+					fwrite($conexao, "\r\n", 512).'<br>';
 					fgets($conexao, 512);
-				} elseif ($dados['2'] == 'mensagem') {
-					fwrite($conexao, $dados['3'] . "\r\n.\r\n") . '<br>';
-					fgets($conexao);
-				} else {
-					fwrite($conexao, $dados['2'] . $dados['3'] . "\r\n", 512) . '<br>';
-					fgets($conexao, 512);
+				} elseif ($dados['2'] == 'mensagem') 
+					{
+						fwrite($conexao, $dados['3']."\r\n.\r\n").'<br>';
+						fgets($conexao);
+					} else {
+						fwrite($conexao, $dados['2'].$dados['3']."\r\n", 512).'<br>';
+						fgets($conexao, 512);
+					}
 				}
+				//print_r($dados);
+				//echo '<HR>';
 			}
-			//print_r($dados);
-			//echo '<HR>';
-		}
-		fclose($conexao);
-		return (1);
+			fclose($conexao);
+			return(1);	
 	}
 
 	function email_check($chemail) {
