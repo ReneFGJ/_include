2e2eee7f6788a4a05d49fef856a83d1403410a1e
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2015-04-01 18:38:17 -0300

    v0.15.14a
    - Classe email novo (incompleta)

--- sisdoc_email_new.php
+++ sisdoc_email_new.php
@@ -3,16 +3,16 @@
  * Sistema de e-mail
  */
 $admin_nome = 'Rene F. Gabriel';
-$email_adm =  'rene.gabriel@pucpr.br';
+$email_adm = 'rene.gabriel@pucpr.br';
 
 require_once ("libs/email/PHPMailerAutoload.php");
 
 function enviaremail($para, $blank, $titulo, $texto) {
 	$mail = new mail;
-	$mail->titulo = $titulo;
-	$mail->texto = $texto;
-	$mail->to = array('renefgj@gmail.com');
-	$mail->method_2_mail(); 
+	$mail -> titulo = $titulo;
+	$mail -> texto = $texto;
+	$mail -> to = array('renefgj@gmail.com');
+	$mail -> method_2_mail();
 }
 
 class mail {
@@ -21,18 +21,18 @@ class mail {
 	var $to = array();
 	var $cc = array();
 	var $cco = array();
-	var $email='rene@fonzaghi.com.br';
-	var $email_replay='';
-	var $email_pass = '448545ct';
+	var $email = 'rene@sisdoc.com.br';
+	var $email_replay = '';
+	var $email_pass = 'Viviane@1970';
 	var $email_name = 'Rene F. Gabriel Jr.';
-	var $email_smtp = 'mail.fonzaghi.com.br';
+	var $email_smtp = 'mail.sisdoc.com.br';
 	var $debug = 1;
-	
+
 	function method_1_mail() {
 		global $admin_nome, $email_adm;
-		$to = $this->to[0];
-		$body = $this->texto;
-		$subject = $this->titulo;
+		$to = $this -> to[0];
+		$body = $this -> texto;
+		$subject = $this -> titulo;
 
 		$headers = '';
 		$headers .= "To: " . $e1 . " \n";
@@ -54,7 +54,7 @@ class mail {
 		$headers = 'MIME-Version: 1.0' . "\r\n";
 		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 		$headers .= "From: " . $admin_nome . " <" . $email_adm . "> \r\n";
-		
+
 		//	$body = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">'."\n".$body;
 		if (mail($to, $subject, $body, $headers)) {
 			return ('OK');
@@ -63,54 +63,53 @@ class mail {
 		}
 	}
 
-	function method_2_mail()
-		{
-			$mail = new PHPMailer;
-			$mail -> isSMTP();
+	function method_2_mail() {
+		$mail = new PHPMailer;
+		$mail -> isSMTP();
 
-			$smtp = trim($this -> email_smtp);
-			$from = trim($this -> email);
-			$replay = trim($this -> email_replay);
-			$pass = trim($this -> email_pass);
-			$from_name = $this -> email_name;
-			$email_to = $this->to[0];
+		$smtp = trim($this -> email_smtp);
+		$from = trim($this -> email);
+		$replay = trim($this -> email_replay);
+		$pass = trim($this -> email_pass);
+		$from_name = $this -> email_name;
+		$email_to = $this -> to[0];
 
-			$mail -> SMTPDebug = $this->debug;
-			$mail -> Debugoutput = 'html';
-			$mail -> Host = $smtp;
-			$mail -> Port = 25;
-			$mail -> SMTPAuth = true;
-			$mail -> Username = $from;
-			$mail -> Password = $pass;
-			$mail -> setFrom($from, $from_name);
-			
-			$mail -> Subject = 'OLA';
-			
-			$mail -> FromName = $from;
-			$mail -> From = $from;			
+		$mail -> SMTPDebug = $this -> debug;
+		$mail -> Debugoutput = 'html';
+		$mail -> Host = $smtp;
+		$mail -> Port = 25;
+		$mail -> SMTPAuth = true;
+		$mail -> Username = $from;
+		$mail -> Password = $pass;
+		$mail -> setFrom($from, $from_name);
+		$mail -> SMTPDebug = true;
 
-			if (strlen($replay) > 0)
-				{
-					$mail -> addReplyTo($from, $from_name);
-				} else {
-					$mail -> addReplyTo($from, $from);		
-				}
-			
-			$mail -> addAddress($to, 'renefgj@gmail.com');
-			
-			$mail -> msgHTML($messagem, dirname(__FILE__));
-			$mail -> AltBody = 'This is a plain-text message body';
-			
-			echo '<pre>';
-			print_r($mail);
-			echo '</pre>';
-			
-			if (!$mail -> send()) {
-				//echo "Mailer Error: " . $mail -> ErrorInfo;
-			} else {
-				//echo "Message sent!";
-			}			
+		$mail -> Subject = 'OLA';
+
+		$mail -> FromName = $from;
+		$mail -> From = $from;
+
+		if (strlen($replay) > 0) {
+			$mail -> addReplyTo($email_replay, $from_name);
+		} else {
+			$mail -> addReplyTo($from, $from_name);
 		}
+
+		$mail -> addAddress($to, 'renefgj@gmail.com');
+
+		$mail -> msgHTML($messagem, dirname(__FILE__));
+		$mail -> AltBody = 'This is a plain-text message body';
+
+		echo '<pre>';
+		print_r($mail);
+		echo '</pre>';
+
+		if (!$mail -> send()) {
+			//echo "Mailer Error: " . $mail -> ErrorInfo;
+		} else {
+			//echo "Message sent!";
+		}
+	}
 
 	function structure() {
 		$sql = "
