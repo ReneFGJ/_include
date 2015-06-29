9222aa7a2850d19f4744f5128a2e9de855929ffd
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2015-06-29 02:07:30 -0300

    v0.15.28
    - Include CodeIgniter

---
+++ codeigniter/install_sisdocform.bat
@@ -0,0 +1,10 @@
+echo **************************************
+echo * Install sisdoc From ****************
+echo * Version 0.15.27 ********************
+echo * Author Rene F. Gabriel Junior ******
+echo **************************************
+copy ../_include/codeigniter/form_sisdoc_helper.php application/helper/form_sisdoc_helper.php
+mkdir css
+copy ../_include/codeigniter/style_form.css css/style_form.css
+pause
+wait
